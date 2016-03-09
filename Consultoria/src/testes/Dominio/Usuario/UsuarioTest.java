266973f42cfb4be085b8dd106e4de325a33ddb7b
Leandro <Leandro@Leandro-NB>
2016-03-09 20:16:59 -0300

    gets da classe Usuário

--- Consultoria/src/testes/Dominio/Usuario/UsuarioTest.java
+++ Consultoria/src/testes/Dominio/Usuario/UsuarioTest.java
@@ -1,378 +1,271 @@
-/**
- * 
- */
 package testes.Dominio.Usuario;
 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 
-import Dominio.Pergunta.Categoria;
 import Dominio.Pergunta.Pergunta;
 import Dominio.Resposta.Resposta;
+import Dominio.Usuario.TipoUsuario;
 import Dominio.Usuario.Usuario;
 
-/**
- * @author Leandro
- *
- */
-@RunWith(MockitoJUnitRunner.class)
+
 public class UsuarioTest {
-	@Mock
-	private String agencia;
 
-	@Mock
-	private int banco;
 
-	@Mock
-	private int conta;
-
-	@Mock
-	private String cpfcnpj;
-
-	@Mock
-	private Date dataNascimento;
-
-	@Mock
-	private char digitoContaCorrente;
-
-	@Mock
-	private String email;
-
-	@Mock
-	private int feedBack;
-
-	@Mock
-	private byte foto;
-
-	@Mock
-	private long id;
-
-	@Mock
-	private Categoria m_Categoria;
-
-	@Mock
-	private String nome;
-
-	@Mock
-	private int numeroResidencia;
-
-	@Mock
-	private List<Pergunta> perguntas;
-
-	@Mock
-	private List<Resposta> respostas;
-
-	@Mock
-	private long telefone;
-
-	@Mock
-	private long telefone2;
-
-	@Mock
-	private int tipoUsuario;
-	@InjectMocks
-	private Usuario usuario;
-
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#getAgencia()}.
-	 */
+	private static final int NUMERO_RESIDENCIA = 121;
+	private static final int ID = 1;
+	private static final int BANCO = 1;
+	private static final int CONTA = 123;
+	@SuppressWarnings("deprecation")
+	private static final Date DATA_NASCIMENTO = new Date(1986, 05, 18);
+	private static final int TELEFONE = 1236985;
+	private static final int TELEFONE2 = 123654987;
+	private static final byte FOTO = (byte) 2;
+	private static final int FEEDBACK = 1;
+	private static final char DIGITO_CONTA_CORRENTE = '1';
+	private static final int TIPO_USUARIO = TipoUsuario.administrador.ordinal();
+	private static final String USUÁRIO_1 = "usuário_1";
+	private static final String EMAIL_DOMINIO_COM = "email@dominio.com";
+	private static final String CPF = "11111111111";
+	private static final String AGENCIA1 = "Agencia1";
+	Usuario u;
+	Pergunta perguntas;
+	Resposta resposta;
+	
+	@SuppressWarnings("deprecation")
+	@Before
+	public void setup() {
+		u = new Usuario();
+		u.setAgencia(AGENCIA1);
+		u.setBanco(BANCO);
+		u.setConta(CONTA);
+		u.setCpfcnpj(CPF);
+		u.setDataNascimento(DATA_NASCIMENTO);
+		u.setDigitoContaCorrente(DIGITO_CONTA_CORRENTE);
+		u.setEmail(EMAIL_DOMINIO_COM);
+		u.setFeedBack(FEEDBACK);
+		u.setFoto(FOTO);
+		u.setId(ID);
+		u.setNome(USUÁRIO_1);
+		u.setTelefone(TELEFONE);
+		u.setTelefone2(TELEFONE2);
+		u.setTipoUsuario(TIPO_USUARIO);
+		u.setNumeroResidencia(NUMERO_RESIDENCIA);
+	
+		Pergunta perguntas = new Pergunta();
+		Resposta resposta = new Resposta();
+				
+		perguntas.setAutor(u);
+		perguntas.setDescricao("descrição pergunta");
+		
+		resposta.setDescricao("descrição resposta");
+		ArrayList<Pergunta> per = new ArrayList<Pergunta>();
+		ArrayList<Resposta> resp = new ArrayList<Resposta>();
+		per.add(perguntas);
+		resp.add(resposta);
+		u.setPerguntas(per);
+		u.setRespostas(resp);		
+	}
+	
+	@After
+	public void tearDown(){
+		u = null;
+	}
+	
 	@Test
 	public void testGetAgencia() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		assertEquals(u.getAgencia(), AGENCIA1);
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#setAgencia(java.lang.String)}.
-	 */
 	@Test
 	public void testSetAgencia() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#getBanco()}.
-	 */
 	@Test
 	public void testGetBanco() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		assertEquals(u.getBanco(), BANCO);
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#setBanco(int)}.
-	 */
 	@Test
 	public void testSetBanco() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#getConta()}.
-	 */
 	@Test
 	public void testGetConta() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		assertEquals(u.getConta(), CONTA);
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#setConta(int)}.
-	 */
 	@Test
 	public void testSetConta() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#getCpfcnpj()}.
-	 */
 	@Test
 	public void testGetCpfcnpj() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		assertEquals(u.getCpfcnpj(), CPF);
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#setCpfcnpj(java.lang.String)}.
-	 */
 	@Test
 	public void testSetCpfcnpj() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#getDataNascimento()}.
-	 */
 	@Test
 	public void testGetDataNascimento() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		assertEquals(u.getDataNascimento(), DATA_NASCIMENTO);
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#setDataNascimento(java.util.Date)}.
-	 */
 	@Test
 	public void testSetDataNascimento() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#getDigitoContaCorrente()}.
-	 */
 	@Test
 	public void testGetDigitoContaCorrente() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		assertEquals(u.getDigitoContaCorrente(), DIGITO_CONTA_CORRENTE);
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#setDigitoContaCorrente(char)}.
-	 */
 	@Test
 	public void testSetDigitoContaCorrente() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#getEmail()}.
-	 */
 	@Test
 	public void testGetEmail() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		assertEquals(u.getEmail(), EMAIL_DOMINIO_COM);
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#setEmail(java.lang.String)}.
-	 */
 	@Test
 	public void testSetEmail() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#getFeedBack()}.
-	 */
 	@Test
 	public void testGetFeedBack() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		assertEquals(u.getFeedBack(), FEEDBACK);
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#setFeedBack(int)}.
-	 */
 	@Test
 	public void testSetFeedBack() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#getFoto()}.
-	 */
 	@Test
 	public void testGetFoto() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		assertEquals(u.getFoto(), FOTO);
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#setFoto(byte)}.
-	 */
 	@Test
 	public void testSetFoto() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#getId()}.
-	 */
 	@Test
 	public void testGetId() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		assertEquals(u.getId(), ID);
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#setId(long)}.
-	 */
 	@Test
 	public void testSetId() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#getNome()}.
-	 */
 	@Test
 	public void testGetNome() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		assertEquals(u.getNome(), USUÁRIO_1);
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#setNome(java.lang.String)}.
-	 */
 	@Test
 	public void testSetNome() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#getNumeroResidencia()}.
-	 */
 	@Test
 	public void testGetNumeroResidencia() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		assertEquals(u.getNumeroResidencia(), NUMERO_RESIDENCIA);
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#setNumeroResidencia(int)}.
-	 */
 	@Test
 	public void testSetNumeroResidencia() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#getPerguntas()}.
-	 */
 	@Test
 	public void testGetPerguntas() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#setPerguntas(java.util.List)}.
-	 */
 	@Test
 	public void testSetPerguntas() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#getRespostas()}.
-	 */
 	@Test
 	public void testGetRespostas() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#setRespostas(java.util.List)}.
-	 */
 	@Test
 	public void testSetRespostas() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#getTelefone()}.
-	 */
 	@Test
 	public void testGetTelefone() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		assertEquals(u.getTelefone(), TELEFONE);
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#setTelefone(long)}.
-	 */
 	@Test
 	public void testSetTelefone() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#getTelefone2()}.
-	 */
 	@Test
 	public void testGetTelefone2() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		assertEquals(u.getTelefone2(), TELEFONE2);
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#setTelefone2(long)}.
-	 */
 	@Test
 	public void testSetTelefone2() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#getTipoUsuario()}.
-	 */
 	@Test
 	public void testGetTipoUsuario() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		assertEquals(u.getTipoUsuario(), TIPO_USUARIO);
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#setTipoUsuario(int)}.
-	 */
 	@Test
 	public void testSetTipoUsuario() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#getM_Categoria()}.
-	 */
 	@Test
 	public void testGetM_Categoria() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#setM_Categoria(Dominio.Pergunta.Categoria)}.
-	 */
 	@Test
 	public void testSetM_Categoria() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
-	/**
-	 * Test method for {@link Dominio.Usuario.Usuario#Usuario()}.
-	 */
 	@Test
 	public void testUsuario() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		
 	}
 
 }
